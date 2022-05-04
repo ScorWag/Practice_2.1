047fa3d58abf4adcb7193ca74b84ca26f9a67106
ScorWag <scorwag@rambler.ru>
2022-05-04 17:09:51 +0300

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        float ticketPrice = 4500.78F;
+        int countBonus = 20;
+        int bonus = (int) ticketPrice / countBonus;
+        System.out.println("bonus" + " = " + bonus + " mile");
+    }
+}
+
